b03fa07a90ab8ff7b559bb1d2a630697fc1220b9
vmqxdv <vquintilio@gmail.com>
2025-03-13 14:35:54 +0100

    test: Controllo corretta funzione codice

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Attraverso l'apposita API di Boolean
+ * https://flynn.boolean.careers/exercises/api/random/mail
+ * generare 10 indirizzi email e stamparli in pagina all'internodi una lista.
+ * 
+ */
+
+const endpoing = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+axios.get(endpoing)
+  .then(result => console.log(result));
